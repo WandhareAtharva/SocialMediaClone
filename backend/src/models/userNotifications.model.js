65694d1922735877483fe721488bf9511eb6e4d3
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-26 06:35:23 +0000

    Add userNotifications model with schema and methods

---
+++ backend/src/models/userNotifications.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userNotificationsSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+    },
+    emailNotifications: {
+        type: Boolean,
+        default: true,
+    },
+    pushNotifications: {
+        type: Boolean,
+        default: true,
+    },
+    notificationMessages: {
+        type: Boolean,
+        default: true,
+    },
+}, { timestamps: true });
+
+userNotificationsSchema.index({ userId: 1 }, { unique: true });
+
+export default mongoose.model("UserNotifications", userNotificationsSchema);
