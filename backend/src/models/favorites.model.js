c68913aab5cf108147f33673a3cbd6db0ffe44a2
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-26 06:17:35 +0000

    Add favorites model with schema and methods

---
+++ backend/src/models/favorites.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const favoritesSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    tweetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+        required: true,
+    },
+    favoritedAt: {
+        type: Date,
+        default: Date.now,
+    }
+}, { timestamps: true });
+
+favoritesSchema.index({ userId: 1, tweetId: 1 }, { unique: true });
+
+favoritesSchema.methods.toJSON = function () {
+    const favorite = this;
+    const favoriteObject = favorite.toObject();
+
+    delete favoriteObject.__v;
+
+    return favoriteObject;
+}
+
+export default mongoose.model("Favorites", favoritesSchema);
