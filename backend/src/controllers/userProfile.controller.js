5e6160243b62b04712aebdf995b488d98d3d642b
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-27 21:42:42 +0530

    Refactor userProfile.controller.js to include getUserProfile functionality

--- backend/src/controllers/userProfile.controller.js
+++ backend/src/controllers/userProfile.controller.js
@@ -6,16 +6,40 @@ import { UserProfile } from "../models/userProfile.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
+import { get } from "mongoose";
 
 const userProfileController = {
 
+    getUserProfile: asyncHandler(async (req, res) => {
+        const userProfile = await UserProfile.findOne({ user: req.user._id });
+        if (!userProfile) {
+            return next(new ApiError(404, "User Profile not found"));
+        }
+        const response = res.status(200).json(new ApiResponse(200, userProfile, "User Profile"));
+        return response;
+    }),
+
     updateUserProfile: asyncHandler(async (req, res) => {
-        const {fullName, description, location, website, birthday } = req.body;
-        if ([fullName, description, location, website, birthday].includes("")) {
+        const { fullName, description, location, website, birthday, gender } = req.body;
+        if ([fullName, description, location, website, gender].includes("")) {
             return next(new ApiError(400, "All fields are required"));
         }
 
-        const response = res.status(201).json(new ApiResponse(201, createdUserProfile, "User Profile Created"));
+        const userProfile = await UserProfile.findOne({ user: req.user._id });
+        if (!userProfile) {
+            return next(new ApiError(404, "User Profile not found"));
+        }
+
+        if (fullName !== undefined) userProfile.fullName = fullName;
+        if (description !== undefined) userProfile.description = description;
+        if (location !== undefined) userProfile.location = location;
+        if (website !== undefined) userProfile.website = website;
+        if (birthday !== undefined) userProfile.birthday = birthday;
+        if (gender !== undefined) userProfile.gender = gender;
+
+        const updatedUserProfile = await userProfile.save();
+
+        const response = res.status(201).json(new ApiResponse(201, updatedUserProfile, "User Profile Created"));
         console.log("User Profile Created");
         return response;
     }),
