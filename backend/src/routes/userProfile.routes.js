9c8f66c003987a7e1099a3bc08db6c325ad93d8a
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-27 22:05:58 +0530

    Refactor userProfile.routes.js to remove unused import of multer.middleware.js

--- backend/src/routes/userProfile.routes.js
+++ backend/src/routes/userProfile.routes.js
@@ -2,7 +2,6 @@ console.log('Loaded: userProfile.routes.js File');
 import { Router } from 'express';
 import userProfileController from '../controllers/userProfile.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
-import { upload } from '../middleware/multer.middleware.js';
 
 const router = Router();
 
