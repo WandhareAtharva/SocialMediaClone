421c401dda93b7f3d9c05808c61c1732a877d2ef
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-27 04:00:39 +0000

    Add multer middleware for file upload

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/temp/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+            return cb(new Error('Only images are allowed'));
+        }
+        cb(null, true);
+    }
+})
