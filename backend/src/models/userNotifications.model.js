82a0e5a0766af9eab915e2087ea226c97f7599eb
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-27 22:31:52 +0530

    Refactor userNotifications.model.js to update notification settings and add methods for turning notifications on and off

--- backend/src/models/userNotifications.model.js
+++ backend/src/models/userNotifications.model.js
@@ -43,4 +43,38 @@ const userNotificationsSchema = new mongoose.Schema({
 
 userNotificationsSchema.index({ userId: 1 }, { unique: true });
 
+userNotificationsSchema.pre('save', function (next) {
+    this.totalNotifications = this.notifications.length;
+    next();
+});
+
+userNotificationsSchema.pre('updateOne', function (next) {
+    this.totalNotifications = this.notifications.length;
+    next();
+});
+
+userNotificationsSchema.methods.emailNotificationsOff = function () {
+    this.emailNotifications = false;
+};
+
+userNotificationsSchema.methods.pushNotificationsOff = function () {
+    this.pushNotifications = false;
+};
+
+userNotificationsSchema.methods.notificationMessagesOff = function () {
+    this.notificationMessages = false;
+};
+
+userNotificationsSchema.methods.emailNotificationsOn = function () {
+    this.emailNotifications = true;
+};
+
+userNotificationsSchema.methods.pushNotificationsOn = function () {
+    this.pushNotifications = true;
+};
+
+userNotificationsSchema.methods.notificationMessagesOn = function () {
+    this.notificationMessages = true;
+};
+
 export default mongoose.model("UserNotifications", userNotificationsSchema);
