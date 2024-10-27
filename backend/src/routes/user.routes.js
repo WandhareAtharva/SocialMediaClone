4fb72f6f4a878603cb07686047e6fe6ba0725b3f
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-27 08:58:10 +0530

    Refactor user.routes.js to add secure routes for getting user profile, updating profile, deleting user, getting user by ID, changing password, and refreshing token

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -10,5 +10,11 @@ router.route('/login').post(userController.login);
 // Secure Routes
 router.route('/get-users').get(verifyJWT, userController.getAllUsers);
 router.route('/logout').post(verifyJWT, userController.logout);
+router.route('/get-user').get(verifyJWT, userController.getProfile);
+router.route('/update-user-profile').put(verifyJWT, userController.updateProfile);
+router.route('/delete-user').delete(verifyJWT, userController.deleteUser);
+router.route('/get-user-by-id/:id').get(verifyJWT, userController.getUserById);
+router.route('/change-password').put(verifyJWT, userController.changePassword);
+router.route('/refresh-token').post(userController.refreshAuthToken);
 
 export default router;
