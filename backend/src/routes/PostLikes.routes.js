a540a751e0391136fde093a6d030f61ad5127aa9
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-30 02:09:46 +0530

    Refactor postLikesController and PostLikes.routes.js to update userId field in postLikesSchema to an array and modify the route to use tweetId instead of id

--- backend/src/routes/PostLikes.routes.js
+++ backend/src/routes/PostLikes.routes.js
@@ -1,15 +1,13 @@
 console.log('Loaded: PostLikes.routes.js File');
 import { verifyJWT } from "../middleware/auth.middleware";
 import { Router } from "express";
-import postLikesController from "../controllers/PostLikes.controller";
+import postLikesController from "../controllers/postLikes.controller.js";
 
 const router = Router();
 
-router.route('/')
-    .post(verifyJWT, postLikesController.createPostLike)
-    .get(verifyJWT, postLikesController.getPostLikes);
-router.route('/:id')
+router.route('/:tweetId')
     .get(verifyJWT, postLikesController.getPostLike)
+    .post(verifyJWT, postLikesController.createPostLike)
     .delete(verifyJWT, postLikesController.deletePostLike);
 
 export default router;
