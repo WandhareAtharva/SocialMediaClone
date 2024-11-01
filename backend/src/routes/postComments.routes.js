0f770768a3047ff5f825e2e75e56bc312ba2c6d8
AtharvaWandhare <atharvawandhare@gmail.com>
2024-11-01 16:30:10 +0530

    Add JWT verification middleware to post comments routes

--- backend/src/routes/postComments.routes.js
+++ backend/src/routes/postComments.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import postCommentsController from "../controllers/postComments.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route('/:tweetId')
-    .post(postCommentsController.createComment)
-    .get(postCommentsController.getComments)
-    .delete(postCommentsController.deleteComment)
-    .put(postCommentsController.updateComment)
-    .patch(postCommentsController.updateLikes);
+    .post(verifyJWT, postCommentsController.createComment)
+    .get(verifyJWT, postCommentsController.getComments)
+    .delete(verifyJWT, postCommentsController.deleteComment)
+    .put(verifyJWT, postCommentsController.updateComment)
+    .patch(verifyJWT, postCommentsController.updateLikes);
 
 export default router;
