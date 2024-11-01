46c6a45a276d10ffc6e7fb3aa7c67420b635dee6
AtharvaWandhare <atharvawandhare@gmail.com>
2024-11-01 16:29:52 +0530

    Add favorites controller for managing user favorites on tweets

---
+++ backend/src/controllers/favorites.controller.js
@@ -0,0 +1,40 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import Favorite from "../models/favorite.model";
+
+const favoritesController = {
+    add: asyncHandler(async (req, res) => {
+        const { tweetId } = req.params;
+        const userId = req.user._id;
+        if (!tweetId) throw new ApiError(400, 'Tweet ID is required');
+        if (!userId) throw new ApiError(400, 'User ID is required');
+
+        const favorite = await Favorite.findOne({ userId });
+        if (!favorite) {
+            const newFavorite = Favorite.create({ userId, tweetId });
+            if (!newFavorite) throw new ApiError(500, 'Failed to create favorite');
+            return res.status(201).json(new ApiResponse(201, newFavorite, 'Favorites created and added successfully'));
+        }
+        favorite.tweetId.push(tweetId);
+    }),
+
+    remove: asyncHandler(async (req, res) => {
+        const { tweetId } = req.params;
+        const userId = req.user._id;
+        if (!tweetId) throw new ApiError(400, 'Tweet ID is required');
+        if (!userId) throw new ApiError(400, 'User ID is required');
+
+        const favorite = await Favorite.findOne({ userId });
+        if (!favorite) {
+            const newFavorite = Favorite.create({ userId });
+            if (!newFavorite) throw new ApiError(500, 'Failed to create favorite');
+            return res.status(201).json(new ApiResponse(201, newFavorite, 'Favorites created and added successfully'));
+        }
+        favorite.tweetId.pull(tweetId);
+        await favorite.save();
+        return res.status(200).json(new ApiResponse(200, favorite, 'Favorite removed successfully'));
+    })
+};
+
+export default favoritesController;
