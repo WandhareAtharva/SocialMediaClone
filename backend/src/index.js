fc91f7facc225f5f4673314b439af4cf1502ca58
AtharvaWandhare <atharvawandhare@gmail.com>
2024-11-17 09:21:42 +0530

    Refactor server initialization to include Socket.IO support and update dotenv path

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,28 @@
-console.log('Loaded: index.js File');
 import dotenv from 'dotenv';
-dotenv.config({ path: './src/.env' });
+dotenv.config({ path: '../../.env' });
+// dotenv.configDotenv({ path: '../../.env' })
 
-import app from './app.js';
+import { app, io, server } from './app.js';
 import connectDB from './config/db.connect.js';
 
 const PORT = process.env.PORT || 8000;
 
 connectDB()
     .then(() => {
-        app.listen(PORT, () => {
+
+        app.get('/', (req, res) => {
+            res.status(200).send('Server is running');
+        })
+
+        io.on('connection', (socket) => {
+            console.log('Socket connected: ', socket.id);
+
+            socket.on('disconnect', () => {
+                console.log('Socket disconnected: ', socket.id);
+            })
+        })
+
+        server.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
     })
