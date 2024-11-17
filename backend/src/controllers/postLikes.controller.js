230578eefba9f8f2bdecfcfc7d74633de3da9271
AtharvaWandhare <atharvawandhare@gmail.com>
2024-11-17 09:22:02 +0530

    Update import statements in postLikes.controller.js to include file extensions

--- backend/src/controllers/postLikes.controller.js
+++ backend/src/controllers/postLikes.controller.js
@@ -1,7 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler";
-import { ApiError } from "../utils/ApiError";
-import { ApiResponse } from "../utils/ApiResponse";
-import PostLike from "../models/PostLike";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import PostLike from "../models/postLikes.model.js";
 
 const postLikesController = {
     getPostLike: asyncHandler(async (req, res) => {
