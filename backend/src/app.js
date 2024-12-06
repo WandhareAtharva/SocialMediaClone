e97f375fad74cb9d6c62cc392aaefc703b42bef6
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-12-06 19:38:22 +0530

    Integrate session management and authentication with Passport; update CORS configuration and import statements

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,10 @@ import cookieParser from 'cookie-parser';
 import https from 'https';
 import { Server } from 'socket.io';
 import fs from 'fs';
+import passport from 'passport';
+import session from 'cookie-session';
 
 const app = express();
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
 
 const key = fs.readFileSync('../secureKeys/key.pem')
 const cert = fs.readFileSync('../secureKeys/cert.pem')
@@ -28,6 +26,21 @@ app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.json({ limit: '16kb' }));
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(session({
+    secret: process.env.Client_Secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+    keys: [process.env.Client_Secret]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Import Routes
 import userRoutes from './routes/user.routes.js';
@@ -35,11 +48,12 @@ import userProfileRoutes from './routes/userProfile.routes.js';
 import userNotifications from './routes/userNotifications.routes.js';
 import userSettings from './routes/userSettings.routes.js';
 import tweetRoutes from './routes/tweet.router.js';
-import postLikes from './routes/postLikes.routes.js';
-import postViews from './routes/postViews.routes.js';
+import postLikes from './routes/PostLikes.routes.js';
+import postViews from './routes/PostViews.routes.js';
 import postComments from './routes/postComments.routes.js';
 import favorites from './routes/favorites.routes.js';
 import FollowedRelationships from './routes/followedRelationships.routes.js';
+import GoogleAuth from './routes/auth.routes.js';
 
 // Use Routes
 app.use('/api/v1/users', userRoutes);
@@ -52,5 +66,6 @@ app.use('/api/v1/postViews', postViews);
 app.use('/api/v1/postComments', postComments);
 app.use('/api/v1/favorites', favorites);
 app.use('/api/v1/followedRelationships', FollowedRelationships);
+app.use('/auth', GoogleAuth);
 
 export { app, io, server };
