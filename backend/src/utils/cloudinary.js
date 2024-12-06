18e56bfd8a9c7ae995e828ce5b1ec196a9c5bdee
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-12-06 19:39:35 +0530

    Refactor Cloudinary configuration: move console log statement for API key to improve clarity

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,12 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-console.log('CLOUDINARY_CLOUD_API:', process.env.CLOUDINARY_API_KEY);
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+console.log('CLOUDINARY_CLOUD_API:', process.env.CLOUDINARY_API_KEY);
 
 const uploadOnCloudinary = async (localFilePath) => {
     // console.log(`Cloudinary config: ${process.env.CLOUDINARY_CLOUD_NAME}, ${process.env.CLOUDINARY_API_KEY}, ${process.env.CLOUDINARY_API_SECRET}, ${localFilePath}`)
