ecbeeb46ca769e61f2d3ca71c50a53061a3da142
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-12-06 19:40:03 +0530

    Add Dashboard route to main application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 // import './App.css';
 import Landing from './Pages/Landing.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path="/home" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   )
